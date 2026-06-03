ebba26e6550dd68a7277d9caa9be9ecf28a56001
Wentong Liu <vkki@qq.com>
2026-06-04 01:48:08 +0800

    Initial Commit

---
+++ ZhiYu/ZhiYuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhiYuApp.swift
+//  ZhiYu
+//
+//  Created by Wentong Liu on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct ZhiYuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
